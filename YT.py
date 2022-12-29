2570ffc812c302f68e1b7f902faad331c9dea61c
fdchidev <114268148+fdchidev@users.noreply.github.com>
2022-12-29 00:07:08 -0600

    Add files via upload
    script python

---
+++ YT.py
@@ -0,0 +1,22 @@
+# import api pytube
+from pytube import YouTube, Playlist
+
+# YT link to download
+video = YouTube('https://www.youtube.com/shorts/tLE9VB6ICH4')
+
+# Parámetros para la descarga
+video.streams.filter(progressive=True, file_extension='mp4').order_by(
+    'resolution').desc().first().download()
+
+
+# For Download Playlist
+"""
+playlist = Playlist(
+ 'https://www.youtube.com/watch?v=ygBAqpSQRLU&list=PLRb5wMnj65To_GS_ZIpSmjA3Sov-8h-ii')
+
+for video in playlist.videos:
+    print('Descargando {}'.format(video.title))
+    video.streams.filter(progressive=True, file_extension='mp4').order_by(
+    'resolution').desc().first().download()
+    print('-----------------------------------------------')
+"""
